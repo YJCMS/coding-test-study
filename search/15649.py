74fa85c1981920d8eeaf97afe6fdf1d954a209f1
YJCMS <sen9563@gmail.com>
2024-11-20 18:52:07 +0900

    dfs

---
+++ search/15649.py
@@ -0,0 +1,21 @@
+# 백트래킹, dfs
+import sys
+
+def dfs():
+    if len(arr) == m:
+        print(' '.join(map(str, arr)))
+        return
+    for i in range(1, n+1):
+        if visited[i]:
+            continue
+        visited[i] = True
+        arr.append(i)
+        dfs()
+        arr.pop()
+        visited[i] = False
+        
+n, m = map(int, sys.stdin.readline().split())
+arr = []
+visited = [False] * (n+1)
+
+dfs()
